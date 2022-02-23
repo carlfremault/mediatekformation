9adb1d631ce89d7307cbdc76bc62948656a41895
Carl Fremault <carlfremault@yahoo.com>
2022-02-23 17:28:52 +0100

    Phase 9 : Intégration de la liste des formations sur la page admin, avec les mêmes options (tri, filtrage, recherche) que sur la page front, ainsi que les possibilités de modifier, supprimer et ajouter des formations - contrôleur AdminFormationsController: ajout des fonctions sort, filter, findAllContain - contrôleur AdminFormationsController: ajout des fonctions suppr, edit, ajout - modification de l'entité Formation pour permettre de vérifier la taille d'une image dont l'url est saisie - modification de l'entité Formation pour contrôler les saisies (titre ne peut pas être vide, longueur des chaînes) - modification du fichier de configuration twig.yaml pour utiliser le thème bootstrap_4_layout.html.twig pour les formulaires - création de formulaire FormationType pour pouvoir modifier ou ajouter une formations - création d'un template _admin.formation.form.html.twig pour intégrer le formulaire FormationType - création des templates admin.formation.ajout.html.twig et admin.formation.edit.html.twig qui intègrent le template _admin.formation.form.html.twig - modification du template admin.formations.html.twig pour ajouter un bouton qui permet d'ajouter une nouvelle formation
    Task #15 - Construire et gérer le formulaire qui permet de créer et modifier une formation

--- src/Controller/admin/AdminFormationsController.php
+++ src/Controller/admin/AdminFormationsController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\admin;
 
+use App\Entity\Formation;
+use App\Form\FormationType;
 use App\Repository\FormationRepository;
 use App\Repository\NiveauRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +19,9 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AdminFormationsController extends AbstractController {
 
+    /**
+     * @var string
+     */
     private const PAGEADMINFORMATIONS = "admin/admin.formations.html.twig";
         
     /**
@@ -29,22 +35,28 @@ class AdminFormationsController extends AbstractController {
     private $niveauRepository;
 
     /**
-     *
      * @var Niveaux[]
      */
     private $niveaux;
+    
+    
 
     /**
-     * 
+     * @var EntityManagerInterface
+     */
+    private $om;
+            
+    /**
      * @param FormationRepository $formationRepository
      */
-    function __construct(FormationRepository $formationRepository, NiveauRepository $niveauRepository) {
+    function __construct(FormationRepository $formationRepository, NiveauRepository $niveauRepository, EntityManagerInterface $om) {
         $this->formationRepository = $formationRepository;
         $this->niveauRepository = $niveauRepository;
+        $this->om = $om;
         $this->niveaux = $this->niveauRepository->findAll();
     }
     
-        /**
+    /**
      * @Route("/admin", name="admin.formations")
      * @return Response
      */
@@ -55,4 +67,103 @@ class AdminFormationsController extends AbstractController {
                     'niveaux' => $this->niveaux
         ]);
     }
+    
+    /**
+     * @Route("/admin/tri/{champ}/{ordre}", name="admin.formations.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @return Response
+     */
+    public function sort($champ, $ordre): Response {
+        $formations = $this->formationRepository->findAllOrderBy($champ, $ordre);
+        return $this->render(self::PAGEADMINFORMATIONS, [
+                    'formations' => $formations,
+                    'niveaux' => $this->niveaux
+        ]);
+    }
+
+    /**
+     * @Route("/admin/filter/{champ}/{valeur}", name="admin.formations.filter")
+     * @param type $champ
+     * @param type $valeur
+     * @return Response
+     */
+    public function filter($champ, $valeur = null): Response {
+        $formations = $this->formationRepository->findByEqualValue($champ, $valeur);
+        return $this->render(self::PAGEADMINFORMATIONS, [
+                    'formations' => $formations,
+                    'niveaux' => $this->niveaux
+        ]);
+    }
+
+    /**
+     * @Route("/admin/recherche/{champ}", name="admin.formations.findallcontain")
+     * @param type $champ
+     * @param Request $request
+     * @return Response
+     */
+    public function findAllContain($champ, Request $request): Response {
+        if ($this->isCsrfTokenValid('filtre_' . $champ, $request->get('_token'))) {
+            $valeur = $request->get("recherche");
+            $formations = $this->formationRepository->findByContainValue($champ, $valeur);
+            return $this->render(self::PAGEADMINFORMATIONS, [
+                        'formations' => $formations,
+                        'niveaux' => $this->niveaux
+            ]);
+        }
+        return $this->redirectToRoute("admin");
+    }
+    
+    /**
+     * @Route ("/admin/suppr/{id}", name="admin.formation.suppr")
+     * @param Formation $formation
+     * @return Response
+     */
+    public function suppr(Formation $formation): Response {
+        $this->om->remove($formation);
+        $this->om->flush();
+        return $this->redirectToRoute('admin.formations');
+    }
+ 
+    /**
+     * @route ("/admin/edit/{id}", name="admin.formation.edit")
+     * @param Formation $formation
+     * @param Request $request
+     * @return Response
+     */
+    public function edit(Formation $formation, Request $request): Response {
+       $formFormation = $this->createForm(FormationType::class, $formation);
+       $formFormation->handleRequest($request);
+       if($formFormation->isSubmitted() && $formFormation->isValid()){
+           $this->om->flush();
+           return $this->redirectToRoute('admin.formations');
+       }
+        return $this->render("admin/admin.formation.edit.html.twig", [
+            'formation' => $formation,
+            'niveaux'=>$this->niveaux,
+            'formformation'=>$formFormation->createView()
+        ]);
+    }
+    
+    /**
+     * @route ("/admin/ajout", name="admin.formation.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response {
+        $formation = new Formation();
+        $formFormation = $this->createForm(FormationType::class, $formation);
+        $formFormation->handleRequest($request);
+        if ($formFormation->isSubmitted() && $formFormation->isValid()) {
+            $this->om->persist($formation);
+            $this->om->flush();
+            return $this->redirectToRoute('admin.formations');
+        }
+        return $this->render("admin/admin.formation.ajout.html.twig", [
+                    'formation' => $formation,
+                    'niveaux' => $this->niveaux,
+                    'formformation' => $formFormation->createView()
+        ]);
+    }
+
 }
--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -2,16 +2,37 @@
 
 namespace App\Entity;
 
-use DateTime;
-use DateTimeInterface;
 use App\Repository\FormationRepository;
+use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * @ORM\Entity(repositoryClass=FormationRepository::class)
  */
-class Formation
-{
+class Formation {
+
+    /**
+     * @var int
+     */
+    private const MINIATUREWIDTH = 120;
+    
+    /**
+     * @var int
+     */
+    private const MINIATUREHEIGHT = 90;
+    
+    /**
+     * @var int
+     */
+    private const PICTUREWIDTH = 640;
+    
+    /**
+     * @var int
+     */
+    private const PICTUREHEIGHT = 480;
+    
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -20,12 +41,15 @@ class Formation
     private $id;
 
     /**
-     * @ORM\Column(type="datetime", nullable=true)
+     * @Assert\NotBlank
+     * @ORM\Column(type="datetime", nullable=false)
      */
     private $publishedAt;
 
     /**
-     * @ORM\Column(type="string", length=91, nullable=true)
+     * @Assert\NotBlank
+     * @Assert\Length(max=91 ,maxMessage = "Le titre ne peut pas excéder {{ limit }} caractères")
+     * @ORM\Column(type="string", length=91, nullable=false)
      */
     private $title;
 
@@ -35,16 +59,19 @@ class Formation
     private $description;
 
     /**
+     * @Assert\Length(max=46,maxMessage = "L'url de la miniature ne peut pas excéder {{ limit }} caractères")
      * @ORM\Column(type="string", length=46, nullable=true)
      */
     private $miniature;
 
     /**
+     * @Assert\Length(max=48,maxMessage = "L'url de l'image ne peut pas excéder {{ limit }} caractères")
      * @ORM\Column(type="string", length=48, nullable=true)
      */
     private $picture;
 
     /**
+     * @Assert\Length(max=11,maxMessage = "L'identifiant de la vidéo ne peut pas excéder {{ limit }} caractères")
      * @ORM\Column(type="string", length=11, nullable=true)
      */
     private $videoId;
@@ -54,106 +81,146 @@ class Formation
      */
     private $niveau;
 
-    public function getId(): ?int
-    {
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getPublishedAt(): ?DateTimeInterface
-    {
+    public function getPublishedAt(): ?DateTimeInterface {
         return $this->publishedAt;
     }
 
     public function getPublishedAtString(): string {
-        return $this->publishedAt->format('d/m/Y');     
-    }    
-        
-    public function setPublishedAt(?DateTimeInterface $publishedAt): self
-    {
+        return $this->publishedAt->format('d/m/Y');
+    }
+
+    public function setPublishedAt(?DateTimeInterface $publishedAt): self {
         $this->publishedAt = $publishedAt;
 
         return $this;
     }
 
-    public function getTitle(): ?string
-    {
+    public function getTitle(): ?string {
         return $this->title;
     }
 
-    public function setTitle(?string $title): self
-    {
+    public function setTitle(?string $title): self {
         $this->title = $title;
 
         return $this;
     }
 
-    public function getDescription(): ?string
-    {
+    public function getDescription(): ?string {
         return $this->description;
     }
 
-    public function setDescription(?string $description): self
-    {
+    public function setDescription(?string $description): self {
         $this->description = $description;
 
         return $this;
     }
 
-    public function getMiniature(): ?string
-    {
+    public function getMiniature(): ?string {
         return $this->miniature;
     }
 
-    public function setMiniature(?string $miniature): self
-    {
+    public function setMiniature(?string $miniature): self {
         $this->miniature = $miniature;
 
         return $this;
     }
 
-    public function getPicture(): ?string
-    {
+    public function getPicture(): ?string {
         return $this->picture;
     }
 
-    public function setPicture(?string $picture): self
-    {
+    public function setPicture(?string $picture): self {
         $this->picture = $picture;
 
         return $this;
     }
 
-    public function getVideoId(): ?string
-    {
+    public function getVideoId(): ?string {
         return $this->videoId;
     }
 
-    public function setVideoId(?string $videoId): self
-    {
+    public function setVideoId(?string $videoId): self {
         $this->videoId = $videoId;
 
         return $this;
     }
 
-    public function getNiveau(): ?niveau
-    {
+    public function getNiveau(): ?niveau {
         return $this->niveau;
     }
-    
-    public function getNiveauString(): ?string 
-    {
+
+    public function getNiveauString(): ?string {
         return $this->niveau->getLibelle();
     }
-    
-    public function getNiveauId(): ?int
-    {
-        return $this->niveau->getId();
-    }        
 
-    public function setNiveau(?niveau $niveau): self
-    {
+    public function getNiveauId(): ?int {
+        return $this->niveau->getId();
+    }
+
+    public function setNiveau(?niveau $niveau): self {
         $this->niveau = $niveau;
 
         return $this;
     }
+
+    /**
+     * Cette fonction vérifie, quand un Url pour une image a été saisie, s'il s'agit bien d'une image,
+     * puis si sa taille correspond avec les tailles fixées en constantes PICTUREWIDTH, PICTUREHEIGHT*
+     * pour les 'picture', MINIATUREWIDTH, MINIATUREHEIGHT pour les miniatures
+     * @Assert\Callback
+     * @param ExecutionContextInterface $context
+     * @param type $payload
+     */
+    public function validate(ExecutionContextInterface $context, $payload) {
+        $miniatureUrl = $this->getMiniature();
+        $miniatureWidth = 0;
+        $miniatureHeight = 0;
+
+        $pictureUrl = $this->getPicture();
+        $pictureWidth = 0;
+        $pictureHeight = 0;
+
+        if ($miniatureUrl != "") {
+            try {
+                $info = getimagesize($miniatureUrl);
+                $miniatureWidth = $info[0];
+                $miniatureHeight = $info[1];
+            } catch (\Exception $e) {
+                $context->buildViolation("Ceci n'est pas une image")
+                        ->atPath('miniature')
+                        ->addViolation();
+                return;
+            };
+
+            if ($miniatureWidth != self::MINIATUREWIDTH || $miniatureHeight != self::MINIATUREHEIGHT) {
+                $context->buildViolation("Les miniatures doivent être de taille ".self::MINIATUREWIDTH."x".self::MINIATUREHEIGHT." pixels")
+                        ->atPath('miniature')
+                        ->addViolation();
+            }
+        }
+
+        if ($pictureUrl != "") {
+            try {
+                $info = getimagesize($pictureUrl);
+                $pictureWidth = $info[0];
+                $pictureHeight = $info[1];
+            } catch (\Exception $e) {
+                $context->buildViolation("Ceci n'est pas une image")
+                        ->atPath('picture')
+                        ->addViolation();
+                return;
+            };
+
+            if ($pictureWidth > self::PICTUREWIDTH || $pictureHeight > self::PICTUREHEIGHT) {
+                $context->buildViolation("La taille des images ne doit pas dépasser ".self::PICTUREWIDTH."x".self::PICTUREHEIGHT." pixels")
+                        ->atPath('picture')
+                        ->addViolation();
+            }
+        }
+    }
+
 }
